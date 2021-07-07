ae1387e54ff4d4077f2de4b8e85fe7d1306d8ffb
dylanmasschelein <dylanmasschelein@gmail.com>
2021-07-06 22:10:52 -0700

    Further investigate H10 heroku deployment bug

--- server/index.js
+++ server/index.js
@@ -30,4 +30,4 @@ app.use(express.json());
 app.use("/areas", databaseAreas);
 app.use("/user", userRoutes);
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server is listening on PORT ${port}...`));
