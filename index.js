ff2663f43d48e41d203a38f0c28b41e218022330
dylanmasschelein <dylanmasschelein@gmail.com>
2021-07-08 15:28:19 -0700

    App successfully Deployed to Heroku

--- index.js
+++ index.js
@@ -26,20 +26,12 @@ mongoose.connect(uri, {
 
 const PORT = process.env.PORT || 5000;
 
-// app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
 
 app.use("/areas", databaseAreas);
 app.use("/user", userRoutes);
 
-// if (proccess.env.NODE_ENV === production) {
-//   app.use(express.static("../client/build"));
-
-//   app.get("*", (_req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "./client/build")));
 
